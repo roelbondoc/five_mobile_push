f2e522800d2c528850ba7a2059cd07bb2f67675b
James Herdman <james.herdman@nulayer.com>
2011-03-29 11:57:41 -0400

    Make Device's UID private
    No need to make this public at this point in time

--- lib/five_mobile_push/device.rb
+++ lib/five_mobile_push/device.rb
@@ -3,32 +3,30 @@ module FiveMobilePush
 
     VALID_OPTION_KEYS = [:alias, :email]
 
-    attr_accessor :device_uid
-
     def initialize(client, device_uid)
-      @client = client
-      self.device_uid = device_uid
+      @client     = client
+      @device_uid = device_uid
     end
 
     # Registers a device for receiving push notifications from an application.
     # If the device is already registered, this call can update the existing registration details.
     #
     def register(registration_data=nil)
-      options = {:device_id => self.device_uid}
+      options = {:device_id => @device_uid}
       options[:reg_data] = registration_data unless registration_data.nil?
       @client.post 'device/register', options
     end
 
     def resume
-      @client.post 'device/resume', :id_type => FiveMobilePush::DEFAULT_ID_TYPE, :id_value => self.device_uid
+      @client.post 'device/resume', :id_type => FiveMobilePush::DEFAULT_ID_TYPE, :id_value => @device_uid
     end
 
     def suspend
-      @client.post 'device/suspend', :id_type => FiveMobilePush::DEFAULT_ID_TYPE, :id_value => self.device_uid
+      @client.post 'device/suspend', :id_type => FiveMobilePush::DEFAULT_ID_TYPE, :id_value => @device_uid
     end
 
     def unregister
-      @client.post 'device/unregister', :id_type => FiveMobilePush::DEFAULT_ID_TYPE, :id_value => self.device_uid
+      @client.post 'device/unregister', :id_type => FiveMobilePush::DEFAULT_ID_TYPE, :id_value => @device_uid
     end
 
   end
