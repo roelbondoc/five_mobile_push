bbe150da4226fb5418dc145e9e9511bef0b0def8
James Herdman <james.herdman@nulayer.com>
2011-03-30 10:51:02 -0400

    Attempt to send device info when registering

--- lib/five_mobile_push/device.rb
+++ lib/five_mobile_push/device.rb
@@ -8,8 +8,8 @@ module FiveMobilePush
     # @param [String] device_uid The ID of the device being registered.
     #   Maximum of 64 characters.
     def initialize(client, device_uid)
-      @client      = client
-      @device_uid  = device_uid
+      @client     = client
+      @device_uid = device_uid
     end
 
     # Registers a device for receiving push notifications from an application.
@@ -19,9 +19,25 @@ module FiveMobilePush
     # @param [String] registration_data Platform specific device registration
     #   data, e.g. iOS device token. Optional for some platforms
     #
+    # @param [Hash] device_info Information about the device being registered
+    #
+    # @option device_info [String] :manufacturer The device manufacturer.
+    #   E.g. Apple, HTC, Motorola, RIM. Maximum 64 characters.
+    #
+    # @option device_info [String] :model The model of the device. E.g. iPhone 4,
+    #   Nexus One
+    #
+    # @option device_info [String] :platform The software platform. E.g. iOS, Android
+    #
+    # @option device_info [String] :platform_ver Software platform version.
+    #
     # @return [Hash] Has unique device API key. Required for many other calls.
-    def register(registration_data=nil)
-      options = { :device_id => @device_uid }
+    def register(device_info, registration_data=nil)
+      options = {
+        :device_id   => @device_uid,
+        :device_info => MultiJson.encode(device_info)
+      }
+
       options[:reg_data] = registration_data unless registration_data.nil?
       response = @client.post 'device/register', options
       MultiJson.decode(response.body)
--- spec/five_mobile_push/device_spec.rb
+++ spec/five_mobile_push/device_spec.rb
@@ -2,17 +2,30 @@ require 'spec_helper'
 
 describe FiveMobilePush::Device do
 
-  let(:api_token) { 'token' }
+  let(:api_token)       { 'token' }
+
   let(:application_uid) { 'nulayer' }
-  let(:client) { FiveMobilePush::Client.new :api_token => api_token, :application_uid => application_uid }
+
+  let(:client) { FiveMobilePush::Client.new(:api_token => api_token, :application_uid => application_uid) }
+
   let(:device_uid) { '2b6f0cc904d137be2e1730235f5664094b831186' }
+
   let(:device_token) { 'ABCDEFG' }
 
-  subject { FiveMobilePush::Device.new client, device_uid }
+  subject { FiveMobilePush::Device.new(client, device_uid) }
 
   describe '#register' do
 
     let(:register_endpoint) { device_endpoint('register') }
+
+    let(:device_info) {
+      {
+        :manufacturer => 'Apple',
+        :model        => 'iPhone 4',
+        :platform     => 'iOS',
+        :platform_ver => 'iOS 4.3'
+      }
+    }
 
     context "registration data is provided" do
 
@@ -37,10 +50,11 @@ describe FiveMobilePush::Device do
     context "registration data is not provided" do
 
       it "registers a device" do
-        body = build_request_body(:device_id => device_uid)
-        stub_request(:post, register_endpoint).with(:body => body)
-        response = subject.register
-        a_request(:post, register_endpoint).with(:body => body).should have_been_made
+        body = build_request_body(:device_id => device_uid, :device_info => device_info)
+        stub_request(:post, register_endpoint)
+        subject.register(device_info)
+
+        a_request(:post, register_endpoint).with(:body => /reg_data/).should_not have_been_made
       end
 
     end
