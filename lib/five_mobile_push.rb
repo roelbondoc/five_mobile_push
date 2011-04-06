b746193b1e504734943d6cdb8edabe63cc6d453e
James Herdman <james.herdman@nulayer.com>
2011-04-06 16:25:11 -0400

    Valid configuration keys hardcoded to bolster documentation

--- lib/five_mobile_push.rb
+++ lib/five_mobile_push.rb
@@ -13,15 +13,20 @@ module FiveMobilePush
   class GeneralError      < StandardError; end
   class ServerError       < StandardError; end
 
-
-  VALID_OPTION_KEYS = [:api_token, :application_uid]
-
   DEFAULT_ID_TYPE = 'native'
 
-  attr_accessor *VALID_OPTION_KEYS
+  attr_accessor :api_token, :application_uid
 
   attr_writer :adapter
 
+  # @yield [config] Provides a block to conveniently configure the library
+  #
+  # @example Simple usage
+  #
+  #   FiveMobilePush.configure do |config|
+  #     config.api_token       = '12345'
+  #     config.application_uid = 'myfancyapp'
+  #   end
   def configure
     yield self
   end
