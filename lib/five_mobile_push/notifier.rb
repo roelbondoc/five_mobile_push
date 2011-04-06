50136da2e1689c37ee292cff47a7a99051507de0
James Herdman <james.herdman@nulayer.com>
2011-04-06 14:20:25 -0400

    PayloadProxy is renamed Message, and publicly documented
    Renaming helped support semantically meaningful classes, and making the documentation
    public cuts down on work I have to do when documenting in general. Winning!

--- lib/five_mobile_push/notifier.rb
+++ lib/five_mobile_push/notifier.rb
@@ -8,27 +8,45 @@ module FiveMobilePush
     def broadcast(platforms, &block)
       @client.post 'notify/broadcast',
         :platforms => build_platforms_string(platforms),
-        :payload   => capture_payload(&block).to_json
+        :payload   => capture_message(&block).to_json
     end
 
     def notify_devices(devices, &block)
       @client.post 'notify/toDevices',
         :id_type   => FiveMobilePush::DEFAULT_ID_TYPE,
         :id_values => devices.join(','),
-        :payload   => capture_payload(&block).to_json
+        :payload   => capture_message(&block).to_json
     end
 
     def notify_by_tags(platforms, tags, &block)
       @client.post 'notify/toTags',
         :platforms => build_platforms_string(platforms),
         :tags      => tags.join(','),
-        :payload   => capture_payload(&block).to_json
+        :payload   => capture_message(&block).to_json
+    end
+
+    # Simple proxy class for building messages. Do not use this class directly.
+    class Message
+      # @param [String] body The text to send
+      def body(body)
+        @body = body
+      end
+
+      # @param [Hash] meta_data (optional) The optional meta data to send.
+      def meta_data(meta_data)
+        @meta_data = meta_data
+      end
+
+      # @private
+      def to_payload
+        FiveMobilePush::Payload.new(@message, @meta_data)
+      end
     end
 
     private
 
-    def capture_payload(&block)
-      payload_proxy = PayloadProxy.new
+    def capture_message(&block)
+      payload_proxy = Message.new
       block.call(payload_proxy)
       payload_proxy.to_payload
     end
@@ -38,21 +56,6 @@ module FiveMobilePush
         platforms.join(',')
       else
         platforms.to_s
-      end
-    end
-
-    # @private
-    class PayloadProxy
-      def message(message)
-        @message = message
-      end
-
-      def meta_data(meta_data)
-        @meta_data = meta_data
-      end
-
-      def to_payload
-        FiveMobilePush::Payload.new(@message, @meta_data)
       end
     end
 
