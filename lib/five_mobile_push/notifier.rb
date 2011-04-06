1745e6f713f207a15cb6ac3dc73226d39693c7b5
James Herdman <james.herdman@nulayer.com>
2011-04-06 14:20:39 -0400

    Documentation for FiveMobilePush::Notifier

--- lib/five_mobile_push/notifier.rb
+++ lib/five_mobile_push/notifier.rb
@@ -1,16 +1,62 @@
 module FiveMobilePush
+  # @todo Validate provided platforms
+  # @todo Simplify platform selection
   class Notifier
 
+    # The following are platforms supported by the Five Mobile Push platform
+
+    ALL        = "all"
+    IPHONE     = "iphone"
+    BLACKBERRY = "blackberry"
+    ANDROID    = "android"
+
+    # @param [FiveMobilePush::Client] client The Client object to use when
+    #   sending notices
     def initialize(client)
       @client = client
     end
 
+    # Broadcast a notification to one or more platforms of an application.
+    #
+    # == Supported Platforms
+    #
+    # * all
+    # * iphone
+    # * blackberry
+    # * android
+    #
+    # @param [Array<String>, String] platforms Any of the supported platforms.
+    #
+    # @yield [message] Provides a mini-DSL for constructing the message to
+    #   broadcast.
+    #
+    # @yieldparam [Message] message (see Message)
+    #
+    # @example Simple usage
+    #   n = FiveMobilePush::Notifier.new(client)
+    #   n.broadcast(FiveMobilePush::Notifier::ALL) do |message|
+    #     message.body "Downtime this weekend"
+    #   end
     def broadcast(platforms, &block)
       @client.post 'notify/broadcast',
         :platforms => build_platforms_string(platforms),
         :payload   => capture_message(&block).to_json
     end
 
+    # Send a notification to any number of specified devices
+    #
+    # @param [Array<String>] devices A list of device ID values
+    #
+    # @yield [message] Provides a mini-DSL for constructing the message to
+    #   broadcast.
+    #
+    # @yieldparam [Message] message (see Message)
+    #
+    # @example Simple usage
+    #   n = FiveMobilePush::Notifier.new(client)
+    #   n.notify_devices(['1234']) do |message|
+    #     message.body "Downtime this weekend"
+    #   end
     def notify_devices(devices, &block)
       @client.post 'notify/toDevices',
         :id_type   => FiveMobilePush::DEFAULT_ID_TYPE,
@@ -18,6 +64,28 @@ module FiveMobilePush
         :payload   => capture_message(&block).to_json
     end
 
+    # Notifies any device registered with the provided tags.
+    #
+    # == Supported Platforms
+    #
+    # * all
+    # * iphone
+    # * blackberry
+    # * android
+    #
+    # @param [Array<String>, String] platforms Any of the supported platforms.
+    # @param [Array<String>] tags Any tag that is registered
+    #
+    # @yield [message] Provides a mini-DSL for constructing the message to
+    #   broadcast.
+    #
+    # @yieldparam [Message] message (see Message)
+    #
+    # @example Simple usage
+    #   n = FiveMobilePush::Notifier.new(client)
+    #   n.notify_by_tags(FiveMobilePush::Notifier::ALL, ['muffin', 'bacon']) do |message|
+    #     message.body "Downtime this weekend"
+    #   end
     def notify_by_tags(platforms, tags, &block)
       @client.post 'notify/toTags',
         :platforms => build_platforms_string(platforms),
