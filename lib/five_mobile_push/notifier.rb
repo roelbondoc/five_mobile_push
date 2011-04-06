753679746d3d18dbb079109f470243b973b76f69
James Herdman <james.herdman@nulayer.com>
2011-04-06 10:18:30 -0400

    Make 'build_platforms_string' private in Notifier

--- lib/five_mobile_push/notifier.rb
+++ lib/five_mobile_push/notifier.rb
@@ -6,30 +6,33 @@ module FiveMobilePush
     end
 
     def broadcast(platforms, payload)
-      @client.post 'notify/broadcast', :platforms => self.class.build_platforms_string(platforms), :payload => MultiJson.encode(payload)
+      @client.post 'notify/broadcast',
+        :platforms => build_platforms_string(platforms),
+        :payload   => MultiJson.encode(payload)
     end
 
     def notify_devices(devices, payload)
-      @client.post 'notify/toDevices', :id_type => FiveMobilePush::DEFAULT_ID_TYPE, :id_values => devices.join(','), :payload => MultiJson.encode(payload)
+      @client.post 'notify/toDevices',
+        :id_type   => FiveMobilePush::DEFAULT_ID_TYPE,
+        :id_values => devices.join(','),
+        :payload   => MultiJson.encode(payload)
     end
 
     def notify_by_tags(platforms, tags, payload)
       @client.post 'notify/toTags',
-        :platforms => self.class.build_platforms_string(platforms),
-        :tags => tags.join(','),
-        :payload => MultiJson.encode(payload)
+        :platforms => build_platforms_string(platforms),
+        :tags      => tags.join(','),
+        :payload   => MultiJson.encode(payload)
     end
 
-    class << self
+    private
 
-      def build_platforms_string(platforms)
-        if platforms.kind_of?(Enumerable)
-          platforms.join(',')
-        else
-          platforms.to_s
-        end
+    def build_platforms_string(platforms)
+      if platforms.kind_of?(Enumerable)
+        platforms.join(',')
+      else
+        platforms.to_s
       end
-
     end
 
   end
