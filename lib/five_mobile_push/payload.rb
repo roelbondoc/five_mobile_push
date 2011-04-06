538f4be5c3fe484a835837e2cc30578a8d40348c
James Herdman <james.herdman@nulayer.com>
2011-04-06 10:01:49 -0400

    Document Payload's initializer

--- lib/five_mobile_push/payload.rb
+++ lib/five_mobile_push/payload.rb
@@ -3,6 +3,9 @@ module FiveMobilePush
     attr_reader   :message
     attr_accessor :meta_data
 
+    # @param [#to_s] message The message you wish to send with a notice
+    # @param [Hash] meta_data (nil) Any meta data to send along with the
+    #   notice. Leave as +nil+ if none is to be sent.
     def initialize(message, meta_data=nil)
       self.message   = message
       self.meta_data = meta_data
