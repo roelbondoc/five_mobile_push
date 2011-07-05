d1b6619c000ec88c30e486fe87f319ca0cf3c149
James Herdman <james.herdman@me.com>
2011-07-05 17:47:44 -0400

    Semi-valuable integration test

--- spec/integration/client_spec.rb
+++ spec/integration/client_spec.rb
@@ -1,27 +1,29 @@
 require 'spec_helper'
 
 FiveMobilePush.configure do |config|
-  config.api_token        = 'x'
-  config.application_uid  = 'y'
+  config.api_token        = ''
+  config.application_uid  = ''
 end
 
 describe FiveMobilePush::Client do
   let(:device) { double('Device', id: 12345, uid: 'abcdef') }
+
   let(:device_info) {
     {
       'manufacturer' => 'Apple',
-      'model'        => 'iPod Touch',
-      'platform'     => 'iOS',
+      'model'        => 'iPhone 4',
+      'platform'     => 'iPhone OS',
       'platform_ver' => '4.3.2'
     }
   }
+
   let(:reg_data) { 'cafebabe' }
+
+  subject { described_class.new }
   
   it "register device" do
-    # debugger
-    # a = 1
-    
-    client = FiveMobilePush::Client.new
-    client.device(device.uid).register(device_info, reg_data)
+    expect {
+      subject.device(device.uid).register(device_info, reg_data)
+    }.to_not raise_error
   end
 end
